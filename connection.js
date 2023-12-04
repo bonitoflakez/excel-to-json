8b418982391c0d702e45bb0809298177c46016cf
Bonito :3 <nikhildhiman9050@gmail.com>
2023-12-04 18:53:24 +0530

    feat: render html certificate

--- connection.js
+++ connection.js
@@ -3,7 +3,9 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/?retryWrites=true&w=majority`;
+
+const uri = 'mongodb+srv://exceltojsonuser:exceltojsonuser@exceltojson.su3ewrw.mongodb.net/?retryWrites=true&w=majority'
 
 const connectDB = async () => {
   try {
